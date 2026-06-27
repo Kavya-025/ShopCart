0a86dd5c1768e62a4c01cf1ca4f31375f0054ac4
Kavya <agarwalkavya25aug@gmail.com>
2026-06-27 15:01:30 +0530

    initial

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,14 +6,13 @@ function Header(){
     const {opencart, openAddProd} = useContext(Appcontext);
 
     return (<div className="header">
-        <h1>My react store</h1>
+        <h1>ShopCart</h1>
         <div>
             <button className="yellow-button" style={{marginRight: "20px"}}
                 onClick={openAddProd}> Add Product
             </button>
             <button className="yellow-button" onClick ={opencart}>cart</button>
         </div>
-        {/* <Modal show = {showcart} onclose={closecart}>hello from modal</Modal> */}
     </div>);
 }
 
